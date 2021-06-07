2b0d0e3d1602b476b473961ae4da48030dd6b97b
Roberth0 <rjromerol1999@gmail.com>
2021-06-07 18:46:59 -0500

    load and flip cards

--- memoryGame/main.js
+++ memoryGame/main.js
@@ -1,15 +1,41 @@
 "use strict";
+const gameZone = document.querySelector('.gameZone');
 
-const card = document.querySelector(".card");
-const img = document.querySelector("img");
+// const cardFront = document.querySelector(".card-front");
+// const cardContainer = document.querySelector(".card-container");
+// const cardBack = document.querySelector(".card-back");
+// cardContainer.addEventListener('click', flipCard);
 
-card.addEventListener("click", (e)=>{
-    img.classList.toggle('d-none');
-    card.classList.toggle('bgt')
-    
+const cards = ['dog.svg', 'hippo.svg', 'spider.svg'];
+const completeCards = [...cards, ...cards];
+let score = 0;
 
-    // console.log(e.target.childNodes[0]);
-    // e.target.childNodes[0].classList.toggle("d-none");
-    // e.target.style.backgroundColor = "transparent";
-
+document.addEventListener('click', ()=>{
+    console.log('clicked');
 })
+
+completeCards.forEach( card =>{
+    const cardContainer = document.createElement('div');
+    const cardFront = document.createElement('div');
+    const cardBack = document.createElement('div');
+    cardFront.innerHTML = `<img src="${card}" alt="${card}" >`;
+    cardFront.classList.add('card-front', 'd-none');
+
+    cardBack.classList.add('card-back')
+
+    cardContainer.addEventListener('click', flipCard);
+    cardContainer.classList.add('card-container');
+    cardContainer.append(cardFront);
+    cardContainer.append(cardBack);
+    gameZone.append(cardContainer);
+})
+
+
+
+function flipCard(e){
+    const card = e.target;
+    console.log(e.target);
+    card.childNodes[0].classList.toggle("d-none");
+    card.childNodes[1].classList.toggle("d-none");
+}
+
