4703d139c679810e6990e2d8936217a5d3133c00
Roberth0 <rjromerol1999@gmail.com>
2021-06-28 06:50:18 -0500

    search modal

--- weatherAppVanilla/main.js
+++ weatherAppVanilla/main.js
@@ -1,17 +1,12 @@
 "use strict";
+const searchPlacesBtn = document.getElementById("searchPlacesBtn");
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("closeModal");
 
-const data = document.getElementById("data");
-const value = document.getElementById("value");
-const test = document.getElementById("hi");
-const value = document.getElementById("value");
-const testValue = document.getElementById("testValue");
-const div = document.getElementById("div");
+searchPlacesBtn.addEventListener('click', e =>{
+    modal.classList.toggle("d-none");
+    console.log('works');
+});
 
-const cars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-cars.map(car => console.log(`the number of the car is ${car}`));
-if(value != null){
-    console.log('hi');
-    cars.filter(car => console.log(car > 18));
+closeModal.onclick = ()=>{modal.classList.toggle("d-none")}
     
-}
